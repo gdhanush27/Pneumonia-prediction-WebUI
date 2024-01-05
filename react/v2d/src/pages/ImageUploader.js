cbba2d0044b960ba66855c783aadf1a97b659b1b
Dhanush <dhanushg.21aim@kongu.edu>
2024-01-05 21:02:33 +0530

    2nd model added

--- react/v2d/src/pages/ImageUploader.js
+++ react/v2d/src/pages/ImageUploader.js
@@ -13,15 +13,29 @@ const ImageUploader = () => {
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [responseText, setResponseText] = useState('');
+  const [selectedValue, setSelectedValue] = useState("1"); 
   const [loading, setLoading] = useState(false);
+
+  const handleRadioChange = (value ) => { 
+    setSelectedValue(value); 
+}; 
+
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
   const handleUpload = async () => {
     setLoading(true);
+    if(selectedFile===null)
+    {
+      setLoading(false);
+      setResponseText("Please provide image to proceed..");
+      return;
+    }
     const formData = new FormData();
     formData.append('image', selectedFile);
+    formData.append('model', selectedValue);
+    console.log(formData);
 
     try {
       const response = await axios.post('http://localhost:5000/process_image', formData, {
@@ -43,6 +57,17 @@ const ImageUploader = () => {
         <div className="login-container">
           <label>Upload x-ray image for pneumonia prediction<br/>Supported file types (.jpg , .jpeg) :</label><br/><br/>
       <input type="file" accept=".jpg,.jpeg" onChange={handleFileChange} /><br/>
+      
+
+
+      <input type="radio" id="option1" value="option1" checked={ selectedValue === "1" } 
+        onChange={() => handleRadioChange("1") } /> Pneumonia_detection_model_v1 <br/>
+
+      <input type="radio" id="option1" value="option1" checked={ selectedValue === "2" } 
+        onChange={() => handleRadioChange("2") } /> Pneumonia_detection_model_v2 <br/>
+
+
+
       <button style={{ cursor: loading ? 'not-allowed' : 'pointer' }} onClick={handleUpload} disabled={loading}>Upload and Process Image</button>
       {loading ? <p>Processing...</p> : <p>{responseText}</p>}
       
--- server/app.py
+++ server/app.py
@@ -2,25 +2,23 @@ from flask import Flask, jsonify, request
 import time
 from PIL import Image
 import cv2
-import io
+import os
 import tensorflow as tf
 import numpy as np
 app = Flask(__name__)
+app.config[r'E:\\GIT\\Pneumoia-prediction-WebUI\\server\\uploads'] = 'uploads'
+model1 = tf.keras.models.load_model(r'server\model\pneumonia_detection_model_v1.keras')
+model2 = tf.keras.models.load_model(r'server\model\pneumonia_detection_model_v2.keras')
 
-model = tf.keras.models.load_model(r'server\model\pneumonia_detection_model.keras')
-
-def predict_img(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred_image = cv2.GaussianBlur(gray_image, (7,7), 0)
-    img = cv2.Canny(blurred_image, 10, 30)
-    img = cv2.resize(img, (180, 180))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = np.expand_dims(img, axis=0)
-    p=model.predict(img)
-    if  p < 0.97:
-        return "Person is not affected with Pneumonia."
-    else:
-        return "Person is affected with Pneumonia."
+# def predict_img(image):
+#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     blurred_image = cv2.GaussianBlur(gray_image, (7,7), 0)
+#     img = cv2.Canny(blurred_image, 10, 30)
+#     img = cv2.resize(img, (180, 180))
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     img = np.expand_dims(img, axis=0)
+#     p=model1.predict(img)
+#     return p
 
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
@@ -36,18 +34,23 @@ def process_image():
     if 'image' not in request.files:
         return jsonify({"error": "No image provided"}), 400
     image = request.files['image']
+    model_n = request.form['model']
     image = Image.open(image)
     image = np.array(image)
     blurred_image = cv2.GaussianBlur(image, (7,7), 0)
     canny = cv2.Canny(blurred_image, 10, 30)
     img = cv2.resize(canny, (180, 180))
     img = np.expand_dims(img, axis=0)
-    p=model.predict(img)
-    if  p < 0.97:
-        s="Person is not affected with Pneumonia."
+    if model_n=="1":
+        p=model1.predict(img)
     else:
-        s="Person is affected with Pneumonia."
-    time.sleep(2) #to reduce cpu over load
+        p=model2.predict(img)
+
+    if  p < 0.97:
+        s="Person is not affected with Pneumonia."+" Model Version : "+model_n
+    else:
+        s="Person is affected with Pneumonia."+" Model Version : "+model_n
+
     return jsonify({"text": s})
 
 if __name__ == '__main__':
