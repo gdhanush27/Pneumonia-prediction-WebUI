46078352441c77dc9b516688fe441ea12ff26900
Dhanush <dhanushg.21aim@kongu.edu>
2024-01-06 20:55:48 +0530

    Update config.js

--- react/v2d/src/components/googleSignin/config.js
+++ react/v2d/src/components/googleSignin/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZLDrYx_fyRpwakugfCuesmh2WTKluE0s",
-  authDomain: "watchful-augury-376810.firebaseapp.com",
-  projectId: "watchful-augury-376810",
-  storageBucket: "watchful-augury-376810.appspot.com",
-  messagingSenderId: "1057826426026",
-  appId: "1:1057826426026:web:51b562a84b775401b054d3",
-  measurementId: "G-ZE4RS3XHG0"
+  apiKey: "Replace with actual",
+  authDomain: "Replace with actual",
+  projectId: "Replace with actual",
+  storageBucket: "Replace with actual",
+  messagingSenderId: "Replace with actual",
+  appId: "Replace with actual",
+  measurementId: "Replace with actual"
 };
 
 const app = initializeApp(firebaseConfig);
